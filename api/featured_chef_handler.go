fe2d31340bf700abad7433ee4eb3b3cb67dbefc4
kanedaq <github@kaneda.tech>
2023-07-25 17:41:06 +0900

    ✨以下のAPIにレスポンス型のバリデーションを追加：featuredChef、trendRecipe

--- api/featured_chef_handler.go
+++ api/featured_chef_handler.go
@@ -2,12 +2,13 @@ package api
 
 import (
 	"context"
+	"encoding/json"
 	"math/rand"
 	"net/http"
 
 	"github.com/aopontann/gin-sqlc/docs"
 	"github.com/gin-gonic/gin"
-
+	"github.com/go-playground/validator/v10"
 	"github.com/mattn/go-gimei"
 )
 
@@ -18,15 +19,36 @@ func (s *Server) ListFeaturedChef(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-	if chefs, ok := list.([]docs.FeaturedChef); ok {
-		for i := 0; i < len(chefs); i++ {
-			chefs[i].Name = gimei.NewName().String()
-			chefs[i].NumFollower = rand.Int31n(1000)
-			chefs[i].Score = rand.Int31n(100)
-		}
 
-		c.JSON(http.StatusOK, chefs)
-	} else {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "list is not of type []docs.FeaturedChef"})
+	// ダミーデータ作成（本番では消す）
+	for i := 0; i < len(list); i++ {
+		list[i].Name = gimei.NewName().String()
+		list[i].NumFollower = rand.Int31n(1000)
+		list[i].Score = rand.Int31n(100)
 	}
+
+	// レスポンス型バリデーション
+	validate := validator.New()
+	for i := 0; i < len(list); i++ {
+		// ListFeaturedChefRow型からJSONに変換
+		jsn, err := json.Marshal(list[i])
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		// JSONからdocs.FeaturedChef型に変換
+		var obj docs.FeaturedChef
+		if err := json.Unmarshal(jsn, &obj); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		// バリデーション
+		err = validate.Struct(obj)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, list)
 }
--- api/trend_recipe_handler.go
+++ api/trend_recipe_handler.go
@@ -2,13 +2,13 @@ package api
 
 import (
 	"context"
+	"encoding/json"
 	"math/rand"
 	"net/http"
 
 	"github.com/aopontann/gin-sqlc/docs"
-
 	"github.com/gin-gonic/gin"
-
+	"github.com/go-playground/validator/v10"
 	"github.com/mattn/go-gimei"
 )
 
@@ -20,16 +20,36 @@ func (s *Server) ListTrendRecipe(c *gin.Context) {
 		return
 	}
 
-	if recipe, ok := list.([]docs.TrendRecipe); ok {
-		for i := 0; i < len(list); i++ {
-			list[i].Title = gimei.NewName().First.Katakana()
-			list[i].Introduction = gimei.NewAddress().String() + "。" + gimei.NewAddress().String() + "。" + gimei.NewAddress().String() + "。" + gimei.NewAddress().String() + "。" + gimei.NewAddress().String() + "。"
-			list[i].NumFav = rand.Int31n(1000)
-			list[i].Score = rand.Int31n(100)
-		}
-
-		c.JSON(http.StatusOK, recipe)
-	} else {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "list is not of type []docs.TrendRecipe"})
+	// ダミーデータ作成（本番では消す）
+	for i := 0; i < len(list); i++ {
+		list[i].Title = gimei.NewName().First.Katakana()
+		list[i].Introduction = gimei.NewAddress().String() + "。" + gimei.NewAddress().String() + "。" + gimei.NewAddress().String() + "。" + gimei.NewAddress().String() + "。" + gimei.NewAddress().String() + "。"
+		list[i].NumFav = rand.Int31n(1000)
+		list[i].Score = rand.Int31n(100)
 	}
+
+	// レスポンス型バリデーション
+	validate := validator.New()
+	for i := 0; i < len(list); i++ {
+		// ListTrendRecipeRow型からJSONに変換
+		jsn, err := json.Marshal(list[i])
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		// JSONからdocs.TrendRecipe型に変換
+		var obj docs.TrendRecipe
+		if err := json.Unmarshal(jsn, &obj); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		// バリデーション
+		err = validate.Struct(obj)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, list)
 }
