a7ec9d8a3ad890965e87629097a7786e7fa8d871
aopontann <aopontan0416@gmail.com>
2023-07-26 14:07:23 +0000

    🗃️ ユーザを登録するSQLを作成 #29

--- db/query/user.sql
+++ db/query/user.sql
@@ -1,2 +1,10 @@
 -- name: ExistsUser :one
 SELECT EXISTS (SELECT * FROM usr WHERE email = $1);
+
+-- name: CreateUser :one
+INSERT INTO usr (
+  email, name, auth_server, auth_userinfo
+) VALUES (
+  $1, $2, $3, $4
+)
+RETURNING id, name, email;
--- db/sqlc/user.sql.go
+++ db/sqlc/user.sql.go
@@ -7,7 +7,43 @@ package db
 
 import (
 	"context"
+
+	"github.com/jackc/pgx/v5/pgtype"
 )
+
+const createUser = `-- name: CreateUser :one
+INSERT INTO usr (
+  email, name, auth_server, auth_userinfo
+) VALUES (
+  $1, $2, $3, $4
+)
+RETURNING id, name, email
+`
+
+type CreateUserParams struct {
+	Email        string `json:"email"`
+	Name         string `json:"name"`
+	AuthServer   string `json:"authServer"`
+	AuthUserinfo []byte `json:"authUserinfo"`
+}
+
+type CreateUserRow struct {
+	ID    pgtype.UUID `json:"id"`
+	Name  string      `json:"name"`
+	Email string      `json:"email"`
+}
+
+func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (CreateUserRow, error) {
+	row := q.db.QueryRow(ctx, createUser,
+		arg.Email,
+		arg.Name,
+		arg.AuthServer,
+		arg.AuthUserinfo,
+	)
+	var i CreateUserRow
+	err := row.Scan(&i.ID, &i.Name, &i.Email)
+	return i, err
+}
 
 const existsUser = `-- name: ExistsUser :one
 SELECT EXISTS (SELECT id, email, name, image_url, profile, link, auth_server, auth_userinfo, created_at, updated_at, num_recipe FROM usr WHERE email = $1)
