d840bccc3daff34ccf786ceae1bc71bc670f43bc
aopontann <aopontan0416@gmail.com>
2023-09-25 11:52:24 +0000

    ✨ 存在しないシェフIDを指定したとき、404を返すようにした #70

--- api/chef_handler.go
+++ api/chef_handler.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aopontann/gin-sqlc/docs"
 	"github.com/aopontann/gin-sqlc/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 )
 
 func (s *Server) ListFeaturedChef(c *gin.Context) {
@@ -45,11 +46,16 @@ func (s *Server) GetChef(c *gin.Context) {
 	id, err := utils.StrToUUID(c.Param("id"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 
 	// 問い合わせ処理
 	row, err := s.q.GetChef(context.Background(), id)
 	if err != nil {
+		if err == pgx.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
